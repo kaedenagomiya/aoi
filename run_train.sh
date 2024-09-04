1a749f184bd3c68cd295bde01dd62097a3c421da
kaedenagomiya <nagomiya75328@gmail.com>
2024-09-04 09:01:15 +0900

    [add] add config file for batch train.

--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH -c 16
 #SBATCH --gres=gpu:1
-#SBATCH --time=0-5:00:00
+#SBATCH --time=01-00:00:00
 #SBATCH --mail-user=nagomiya75328@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH -o /work/sora-sa/aoi/logs4run/stdout-%J.out
@@ -11,7 +11,7 @@
 
 EXECDIR="/work/sora-sa/aoi"
 DOCKERENV="env_aoi_latest.sif"
-CONFIG_TRAIN="config_gt_testrun.yaml"
+CONFIG_TRAIN="config_batch_gt_testrun.yaml"
 
 module load singularity cuda/12.2u2
 nvidia-smi

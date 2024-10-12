842f406a89f7509e86d18eeb7d1aff77d4279ad0
kaedenagomiya <nagomiya75328@gmail.com>
2024-10-13 05:28:54 +0900

    [modify] modify set_seed.

--- toybox.py
+++ toybox.py
@@ -10,6 +10,7 @@ import torch
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
